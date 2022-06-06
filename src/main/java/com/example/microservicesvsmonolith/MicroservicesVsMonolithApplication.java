6de892c7404d07e3c82236433899f013d4d489a5
noneXved <bajno.damian96@gmail.com>
2022-06-06 21:05:47 +0200

    Initial commit

---
+++ src/main/java/com/example/microservicesvsmonolith/MicroservicesVsMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesvsmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesVsMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesVsMonolithApplication.class, args);
+	}
+
+}
